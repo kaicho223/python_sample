f57c730d56aa0c85eae6f83a2637d4459332f1e0
kaicho223 <kaicho223@gmail.com>
2019-10-18 19:33:26 +0900

    Update output_python_knock_part2.py
    add new source

--- knock/part2/output_python_knock_part2.py
+++ knock/part2/output_python_knock_part2.py
@@ -4,5 +4,14 @@ import python_knock_part2 as pk
 pkc = pk.PythonKnockPart2()
 
 print("10. 行数のカウント")
-print(pkc.count_use_unix())
+print(pkc.count_row_num())
+print("")
+
+print("11. タブをスペースに置換")
+print(pkc.replace_tab_to_space())
+print("")
+
+print("12. 1列目をcol1.txtに，2列目をcol2.txtに保存")
+pkc.cut_text(1)
+pkc.cut_text(2)
 print("")
